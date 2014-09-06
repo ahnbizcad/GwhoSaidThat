ccde756e6a41423a0b9905ab2cc288a79b046aff
ahnbizcad <ahnbizcad@gmail.com>
2014-09-06 14:43:16 -0700

    serve_static_assets = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,7 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -84,4 +83,9 @@ Rails.application.configure do
 #
 
   config.assets.precompile = ['*.js', '*.css']
+
+#
+
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true #This is bad and slow. Only a temporary solution.
 end
