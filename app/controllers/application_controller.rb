112a74616f05930db84264725922e17c6a1a4946
ahnbizcad <ahnbizcad@gmail.com>
2014-09-07 14:48:56 -0700

    navbar centering, collapsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def authorize_admin
-    redirect_to root_path, alert: 'Access Denied' unless current_user.admin?
-  end
+  before_action :app_count
+  before_action :article_count
   
+  private
+    def authorize_admin
+      redirect_to root_path, alert: 'Access Denied' unless current_user.admin?
+    end
+
+    def app_count
+      @app_count = App.count
+    end
+
+    def article_count
+      @article_count = Article.count
+    end
+
 end
